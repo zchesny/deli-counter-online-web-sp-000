22b3769b0c3fa3d33071b537127f608d62ba870c
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 19:08:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,12 +5,13 @@ def line(line)
     puts "The line is currently empty."
   else
     str = "The line is currently: "
-    line.each_with_index{|name, index| str << "#{index+1}. #{name} "}
+    line.each_with_index{|name, index| str << "#{index + 1}. #{name} "}
     puts str[0...-1]
   end
 end
 
 def take_a_number(line, name)
+  puts "Welcome #{name}. You are number #{line.size + 1} in line."
   line << name
 end
 
